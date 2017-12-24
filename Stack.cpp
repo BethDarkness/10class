aa8f3e38429703cb47925ae6c3e719d499f7b043
BethDarkness <aleksandr.shafir@mail.ru>
2017-12-25 00:45:55 +0300

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -11,36 +11,35 @@ class stack{
     node* head;
     public:
     stack (int a){
-        head=new node;
-        head->data=a;
-        head->next=NULL;
+        head = new node;
+        head -> data = a;
+        head -> next = NULL;
     }
-    ~stack(){
-        while (head!=NULL){
-            node* temp=head;
-            head=head->next;
+    ~stack() {
+        while (head != NULL){
+            node* temp = head;
+            head = head -> next;
             delete temp;
         }
     }
     void push (int a){
-        if (head!=NULL){
-            node* temp=new node;
-            temp->data=a;
-            temp->next=head;
-            head=temp;
+        if (head != NULL){
+            node* temp = new node;
+            temp -> data = a;
+            temp -> next = head;
+            head = temp;
         }
     }
     int pop(){
-        if (head!=NULL){
-            node* temp=head;
-            head=head->next;
-            int a=temp->data;
+        if (head != NULL){
+            node* temp = head;
+            head = head -> next;
+            int a = temp -> data;
             delete temp;
             return a;
         }
     }
 };
-
 
 int main()
 {
