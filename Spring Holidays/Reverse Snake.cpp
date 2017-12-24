34ab62356ecc264bfcd6f5dfac5afd197260c1f7
BethDarkness <aleksandr.shafir@mail.ru>
2017-12-24 21:24:21 +0300

    Update Reverse Snake.cpp

--- Spring Holidays/Reverse Snake.cpp
+++ Spring Holidays/Reverse Snake.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-#include <conio.h>
-
 using namespace std;
 
 void put_snake(int** Array, int lenght_x, int lenght_y) {
-	int y = 0;		//coordinates
+	int y = 0;
 	int x = 0;
-	int counter = lenght_x*lenght_y;
-	while (counter > 1) {		//main cycle
-		for (y = y; y < lenght_y; y++) {		//move down
+	int counter = lenght_x * lenght_y;
+	while (counter > 1) {
+		for (y = y; y < lenght_y; y++) {
 			if (y + 1 == lenght_y) {
 				break;
 			}
@@ -19,7 +17,7 @@ void put_snake(int** Array, int lenght_x, int lenght_y) {
 				Array[y][x] = counter;
 			counter--;
 		}
-		for (x = x; x < lenght_x; x++) {		//move right
+		for (x = x; x < lenght_x; x++) {
 			if (Array[y][x + 1] != 0) {
 				break;
 			}
@@ -30,7 +28,7 @@ void put_snake(int** Array, int lenght_x, int lenght_y) {
 				Array[y][x] = counter;
 			counter--;
 		}
-		for (y = y; y > 0; y--) {				//move up
+		for (y = y; y > 0; y--) {
 			if (Array[y - 1][x] != 0) {
 				break;
 			}
@@ -41,7 +39,7 @@ void put_snake(int** Array, int lenght_x, int lenght_y) {
 				Array[y][x] = counter;
 			counter--;
 		}
-		for (x = x; x > 0; x--) {				//move left
+		for (x = x; x > 0; x--) {
 			if (Array[y][x - 1] != 0) {
 				break;
 			}
@@ -54,7 +52,7 @@ void put_snake(int** Array, int lenght_x, int lenght_y) {
 		}
 
 	}
-	for (int i = 0; i < lenght_y; i++) {			//output array
+	for (int i = 0; i < lenght_y; i++) {
 		for (int j = 0; j < lenght_x; j++) {
 			cout << Array[i][j] << " ";
 		}
@@ -62,16 +60,15 @@ void put_snake(int** Array, int lenght_x, int lenght_y) {
 	}
 }
 
-int main()
-{
+int main() {
 	int lenght_x;
 	int lenght_y;
 	cin >> lenght_y >> lenght_x;
-	int** Array = new int*[lenght_y];			//create Array
+	int** Array = new int*[lenght_y];
 	for (int i = 0; i < lenght_y; i++) {
 		Array[i] = new int[lenght_x];
 	}
-	for (int i = 0; i < lenght_y; i++) {		//fill Array with 0
+	for (int i = 0; i < lenght_y; i++) {
 		for (int j = 0; j < lenght_x; j++) {
 			Array[i][j] = 0;
 		}
