e0e342945d0acfd28610b5710e3c0feb5dc1cdef
BethDarkness <aleksandr.shafir@mail.ru>
2017-12-24 23:19:12 +0300

    Update Unusual Diofant.cpp

--- Winter Holidays/Unusual Diofant.cpp
+++ Winter Holidays/Unusual Diofant.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
-
 using namespace std;
 
 void unusual_diofant(int a, int b, int c);
 
-int main()
-{
-	int a,b,c;																		//zikl, dayushii pravo vvoda do momenta,
-	{																						//poka polzovatelu eto neobxodimo
+int main() {
+	int a,b,c; {
 		cin >> a >> b >> c;
-		if (a%b == 0 || b%a == 0)
-		{
+		if (a % b == 0 || b % a == 0) {
 			cout << "none"<<endl;
 		}
-		else if (a / b < 0 || b / a < 0)
-		{
+		else if (a / b < 0 || b / a < 0) {
 			cout << "inf"<<endl;
 		}
-		else
-		{
+		else {
 			unusual_diofant(a, b, c);
 		}
-
 	}
     return 0;
 }
 
-void unusual_diofant(int a, int b, int c)
-{
+void unusual_diofant(int a, int b, int c) {
 	int x = 0;
 	int y = 0;
 	float Y = 0.0;
-	for (;;)															//vichislenie neotrizhatelnix kornei yravneniia
+	for (;;)
 	{
 		Y = (c - a*static_cast<float>(x)) / b;
 		y = static_cast<int>(Y);
