ac9056f7283127b4ac3f0a055249d66895e66b8c
BethDarkness <aleksandr.shafir@mail.ru>
2017-12-24 21:44:34 +0300

    Update Outer Points.cpp

--- Winter Holidays/Outer Points.cpp
+++ Winter Holidays/Outer Points.cpp
@@ -1,43 +1,35 @@
 #include <iostream>
-
 using namespace std;
 
 void outer_points(int n, int m);
 
-int main()
-{
+int main() {
 	int n, m;
 	cin >> n >> m;
 	outer_points(n, m);
     return 0;
 }
 
-void outer_points(int n, int m)
-{
-	int *prinadlegnost = new int[m];										// peremennaia, nakaplivayuchaia kol-vo otrezkov, v kotorix sod. tochka
+void outer_points(int n, int m) {
+	int *prinadlegnost = new int[m];
 	int *tochka = new int[m];
 	int *a = new int[n];
 	int *b = new int[n];
-	for (int i = 0; i <n; i++)												// vivod otrezkov
-	{
+	for (int i = 0; i <n; i++){
 		cin >> a[i] >> b[i];
 	}
 
-	for (int i = 0; i < m; i++)
-	{
+	for (int i = 0; i < m; i++) {
 		cin >> tochka[i];
 		prinadlegnost[i] = 0;
 	}
-	for (int i = 0; i < m; i++)													// proverka tochek na soderchanie v otrezkax
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (tochka[i] >= a[j] && tochka[i] <= b[j])	prinadlegnost[i] ++;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (tochka[i] >= a[j] && tochka[i] <= b[j])	
+			prinadlegnost[i] ++;
 		}
 	}
-	for (int i = 0; i < m; i++)													// vivod rezyltata
-	{
+	for (int i = 0; i < m; i++) {
 		cout << n - prinadlegnost[i] << " ";
 	}
-
 }
