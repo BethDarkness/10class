6ed70fac889dee19416df0e136ea64f542efec60
BethDarkness <aleksandr.shafir@mail.ru>
2017-12-25 01:07:11 +0300

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -1,53 +1,40 @@
 #include <iostream>
-
 using namespace std;
 
-typedef class
-{
+typedef class{
     char* arr;
     public:
-    string (char* a, int n)
-    {
+    string (char* a, int n) {
         arr = new char**[n];
-        for (int i=0; i<n; i++)
-        {
-            arr[i]=a[i];
+        for (int i = 0; i < n; i++) {
+            arr[i] = a[i];
         }
     }
-    char* get (int i)
-    {
+    char* get (int i) {
         return arr[i];
     }
-    void set (char* a, int i)
-    {
-        arr[i]=a[i];
+    void set (char* a, int i) {
+        arr[i] =a [i];
     }
-    void print()
-    {
-        for (int i=0; i<n; i++)
-        {
-            cout<<arr[i];
+    void print() {
+        for (int i = 0; i < n; i++) {
+            cout << arr[i];
         }
     }
-    ~string()
-    {
-        for (int i=0; i<n; i++)
-        {
+    ~string() {
+        for (int i = 0; i < n; i++) {
             delete[] arr[i];
         }
         delete[] arr;
     }
 } string;
 
-int main ()
-{
+int main () {
     char* a[10];
-    for (int i=0; i<10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         cin>>a[i];
     }
-    string obj (a, 10);
-    {
+    string obj (a, 10); {
         cout<<obj.get(5);
         obj.set(a, 6);
         obj.print();
